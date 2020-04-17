2077ffe72b9b611155206a2190458c2bbbf158f3
闫柯玮 <yankewei@yankeweideMacBook-Air.local>
2020-04-17 13:18:53 +0800

    remove greet

--- src/HelloWorld.php
+++ src/HelloWorld.php
@@ -4,11 +4,6 @@ namespace Yankewei\Composer;
 
 class HelloWorld 
 {
-    public function greet(string $name) 
-    {
-        return "hello {$name}";
-    }
-
     public function greetV2(string $name)
     {
         return "Hello {$name} V2";
